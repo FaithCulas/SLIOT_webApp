14e2a117cabb2c1c348dce82e653afedf229e484
Faith Culas <faithculas@Faiths-MacBook-Pro.local>
2020-10-18 11:36:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './Login/Login'
+import Login from './Login/Login';
+import { Navbar } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar color="blue" variant="dark">
+      SnapIT
+      </Navbar>
+      <Login/>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+   
+export default function NavigationBar(){
+    return(
+    <Navbar bg="dark">
+      SnapIT
+    </Navbar>
+    )
+}
